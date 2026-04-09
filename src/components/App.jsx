1a9729ee488085c9d055929f3ecaa9656fdc91c4
mitra <dimon52152@gmail.com>
2026-04-09 18:28:09 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <main className="card">
+        <header>
+          <h1>Battery Monitoring</h1>
+          <p className="subtitle">Frontend stub</p>
+        </header>
+
+        <section className="content">
+          <p>UI will be implemented here</p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
